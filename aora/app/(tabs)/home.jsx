8f0214acb600ed6e2a81ffe57460f799efa1827d
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-07-17 15:41:55 +0530

    update

--- aora/app/(tabs)/home.jsx
+++ aora/app/(tabs)/home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
                   Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  JSMastery
+                  Aora Social
                 </Text>
               </View>
 
